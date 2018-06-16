73f28332826155225eb693f783c23e90ad79667e
PetarIvancevic <petar.ivancevic171@gmail.com>
2018-06-16 15:48:40 +0200

    added drawer component with login/logout

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import {Text, View} from 'react-native'
 
+import styles from './styles'
+
 export default class Home extends Component<Props> {
   render () {
     return (
-      <View>
+      <View style={styles.body}>
         <Text>Home screen!</Text>
       </View>
     )
